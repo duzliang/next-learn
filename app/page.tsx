735010446e7ea78fe617c3be8d1109f73ad911d0
杜志良 <duzhiliang@feewee.cn>
2023-11-27 18:19:12 +0800

    fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
 import styles from '@/app/ui/home.module.css';
 
 
@@ -16,7 +17,7 @@ export default function Page() {
           {/*  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"*/}
           {/*/>*/}
           <div className={styles.shape}></div>
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
